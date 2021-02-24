5a9037de10a0bcaa4a0e7372f3c71ba396470d87
Marcos Duarte <marcosduarte1994@gmail.com>
2021-02-23 22:47:07 -0300

    Aula 02 com Next.js

---
+++ src/components/Profile/index.tsx
@@ -0,0 +1,16 @@
+import styles from '../../styles/components/Profile.module.css';
+
+export function Profile() {
+  return (
+    <div className={styles.profileContainer}>
+      <img src="https://avatars.githubusercontent.com/u/51549798?s=460&u=4a834e39e36c09f657c13ecf3fddb14b3ca17712&v=4" alt="Marcos Duarte"/>
+      <div>
+        <strong>Marcos Duarte</strong>
+        <p>
+          <img src="icons/level.svg" alt="Level"/>
+        </p>
+        <p>Level 1</p>
+      </div>
+    </div>
+  )
+}
