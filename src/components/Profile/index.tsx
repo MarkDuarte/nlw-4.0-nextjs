793f6ba9d876d1210d8c0a267e9764450207c13f
Marcos Duarte <marcosduarte@Marcoss-MacBook-Air.local>
2021-02-27 11:02:45 -0300

    Continuacao da aplicacao

--- src/components/Profile/index.tsx
+++ src/components/Profile/index.tsx
@@ -1,6 +1,10 @@
+import { useContext } from 'react';
+import { ChallengesContext } from '../../context/ChallengesContext';
 import styles from '../../styles/components/Profile.module.css';
 
 export function Profile() {
+  const {} = useContext(ChallengesContext);
+  
   return (
     <div className={styles.profileContainer}>
       <img src="https://avatars.githubusercontent.com/u/51549798?s=460&u=4a834e39e36c09f657c13ecf3fddb14b3ca17712&v=4" alt="Marcos Duarte"/>
